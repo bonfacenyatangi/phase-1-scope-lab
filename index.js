727b4ba2f47f370e971bee61189254400a43a4c4
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-03-21 11:17:15 +0300

    Done with asssignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob'; // declared in global scope
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase(); // modifies the global customerName variable
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // declared and assigned in global scope
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer; // modifies the global bestCustomer variable
+}
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = 'xyz'; // declared and assigned in local scope
+  leastFavoriteCustomer = 'abc'; // reassigns the local variable, but does not affect global scope
+}
